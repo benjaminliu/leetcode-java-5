c2fc6a3aa5474bf956560c064795f277d8b42f1b
ashley <benjamin.liu@live.cn>
2022-11-14 19:09:08 -0500

    _0027_e_Remove_Element

---
+++ src/com/ben/array/_0027_e_Remove_Element.java
@@ -0,0 +1,20 @@
+package com.ben.array;
+
+public class _0027_e_Remove_Element {
+
+    class Solution {
+        public int removeElement(int[] nums, int val) {
+            int count = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == val) {
+                    count++;
+                } else {
+                    nums[i - count] = nums[i];
+                }
+            }
+
+            return nums.length - count;
+        }
+    }
+}
