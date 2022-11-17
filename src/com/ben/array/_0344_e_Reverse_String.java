97a17fcb1d606d05fe2ab205b128b6122f7e15a7
ashley <benjamin.liu@live.cn>
2022-11-16 19:03:54 -0500

    _0344_e_Reverse_String

---
+++ src/com/ben/array/_0344_e_Reverse_String.java
@@ -0,0 +1,19 @@
+package com.ben.array;
+
+public class _0344_e_Reverse_String {
+
+    class Solution {
+        public void reverseString(char[] s) {
+            int left = 0;
+            int right = s.length-1;
+
+            while (left < right){
+                char tmp = s[left];
+                s[left] = s[right];
+                s[right]=tmp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
